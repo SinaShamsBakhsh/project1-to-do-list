0d6c392b1889e236f2b3c18fdde54e035dc5b7c4
sina <sina.shams.bakhsh01@gmail.com>
2026-09-12 14:37:10 +0330

    feat : add function save_task

--- todo.py
+++ todo.py
@@ -1,9 +1,25 @@
+import os
+from dotenv import load_dotenv
 import argparse
 
+load_dotenv()
+TASKS_FILE = os.environ.get("TASKS_FILE", "tasks.txt")
+
+#------------------------------------------------------------------
+def save_task(task):
+    with open(TASKS_FILE,'w') as task_file:
+        for t in task:
+            task_file.write(t + "\n")
+#------------------------------------------------------------------
+
+
+    
+#------------------------------------------------------------------
 task_list = []
 def add_task(task):
     task_list.append(task)
-    return
+    return task_list
+#------------------------------------------------------------------
 parser = argparse.ArgumentParser()
 subparser = parser.add_subparsers(dest='command',required=True)
 
@@ -14,3 +30,5 @@ args = parser.parse_args()
 
 if args.command == 'add':
     task_list=add_task(args.task)
+    print(task_list)
+    save_task(task_list)
